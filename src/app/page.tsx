e7c139dc2b006e7263ba695b120f2be914c8601d
brayckner <brayckner@gmail.com>
2024-05-30 23:44:24 -0600

    Adding in shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p>Welcome Page</p>
+        <p>App Page</p>
+        <Button>Click Me</Button>
       </div>
     </main>
   );
